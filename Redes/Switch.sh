38ca6ae850067a7c1cf4159e584ddf82e9a87dc0
IannOli <100435787+iannoliver@users.noreply.github.com>
2022-10-17 14:49:23 -0300

    Update Switch.sh

--- Redes/Switch.sh
+++ Redes/Switch.sh
@@ -53,61 +53,6 @@ sw-g03(config)#
 
 
 sw-g03(config)#enable secret 123@senac
-Switch> ----
-Switch>!Modo EXEC privilegiado
-Switch>Enable
-Switch#
-             !configurar DATA/HORA
-			 
-			
-
-
-Switch#clock set 14:30:00 14 october 2022
-Switch# 
-             !modo configuração global
-
-Switch#configure terminal
-Switch(config)#
-                    !altera o nome do switch
-
-Switch(config)#hostname sw-g03
-sw-g03(config)#
-
-                 !habilitar o serviço de criptografia
-				 
-
-
-sw-g03(config)#service password-encryptation
-sw-g03(config)#
-					
-				 !serviço de marcação de data e hora
-
-
-sw-g03(config)#sevice timestamps log datetime msec
-sw-g03(config)#
-				 
-				 !habilitar segurança de acesso
-
-
-sw-g03(config)#login block-for 120 attemps 4 within 60
-sw-g03(config)#
-				 
-				 !Desabilitar o recurso de tradução de nomes
-
-
-sw-g03(config)#no ip domain-lockup
-sw-g03(config)#
-				 
-				 !desafio: procurar na internet imagens do tipo ascii art cisco
-				 !configurar o banner da mensagem do dia
-
-sw-g03(config)#banner motd #Switch do Grupo-03#
-sw-g03(config)#
-				 
-				 !habilitar a segurança do enable
-
-
-sw-g03(config)#enable secret 123@senac
 sw-g03(config)#
 
                 !criar os usuários locais
@@ -136,6 +81,13 @@ sw-g03(config-line)#
 
 sw-g03(config-line)#copy run star
 sw-g03(config-line)#
+
+                    !Habilitar o protocolo de acesso remoto
+                    transport input
+		    
+sw-g03(config-line)#transport input ssh(feito no vty)
+
+
 
 
 
